de2efb692cb8db8a3274d1504dff505b2b790c14
jihyeon121223 <yjh121223@gmail.com>
2022-04-15 09:43:12 +0900

    default페이지 작성

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #git switch -c mainpage
 def index():
-    return render_template('index.html')
+    return render_template('index.html') #처음에 틀면 여기로 들어감
 
 
 if __name__ == '__main__':
